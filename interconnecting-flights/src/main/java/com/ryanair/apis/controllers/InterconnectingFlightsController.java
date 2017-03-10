8c72f5566d653522baad36d28f6a63932f653ee7
Saverio <Saverio@ReD_DeViL>
2017-03-10 01:17:06 +0100

    Use Spring's RestTemplate

--- interconnecting-flights/src/main/java/com/ryanair/apis/controllers/InterconnectingFlightsController.java
+++ interconnecting-flights/src/main/java/com/ryanair/apis/controllers/InterconnectingFlightsController.java
@@ -1,15 +1,11 @@
 package com.ryanair.apis.controllers;
 
-import java.io.IOException;
-
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.ObjectMapper;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
 import com.ryanair.apis.resources.RyanairRouteResource;
 
 @RestController
@@ -17,48 +13,27 @@ public class InterconnectingFlightsController {
 	private static final String ROUTES_ENDPOINT = "https://api.ryanair.com/core/3/routes/";
 	private static final String SCHEDULES_ENDPOINT = "https://api.ryanair.com/timetable/3/schedules/{departure}/{arrival}/years/{year}/months/{month}";
 
-	@RequestMapping("/routes")
-	public String routes(/*
-							 * @RequestParam(value="name", defaultValue="World")
-							 * String name
-							 */) {
-		// Only one time
-		Unirest.setObjectMapper(new ObjectMapper() {
-			private com.fasterxml.jackson.databind.ObjectMapper jacksonObjectMapper = new com.fasterxml.jackson.databind.ObjectMapper();
+	private final RestTemplate restTemplate;
 
-			public <T> T readValue(String value, Class<T> valueType) {
-				try {
-					return jacksonObjectMapper.readValue(value, valueType);
-				} catch (IOException e) {
-					throw new RuntimeException(e);
-				}
-			}
+	@Autowired
+	public InterconnectingFlightsController(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
 
-			public String writeValue(Object value) {
-				try {
-					return jacksonObjectMapper.writeValueAsString(value);
-				} catch (JsonProcessingException e) {
-					throw new RuntimeException(e);
-				}
-			}
-		});
-		Unirest.setTimeouts(10000, 10000);
-		// Response to Object
-		HttpResponse<RyanairRouteResource[]> routesResponse = null;
-		try {
-			routesResponse = Unirest.get(ROUTES_ENDPOINT).asObject(RyanairRouteResource[].class);
-		} catch (UnirestException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	@GetMapping("/interconnections")
+	public RyanairRouteResource[] routes(@RequestParam(value = "departure", required = true) String departure,
+			@RequestParam(value = "arrival", required = true) String arrival,
+			@RequestParam(value = "departureDateTime", required = true) String departureDateTime,
+			@RequestParam(value = "arrivalDateTime", required = true) String arrivalDateTime) {
 
-		RyanairRouteResource[] resources = routesResponse.getBody();
+		// ArrayNode resources = routesResponse.getBody();
 
-		StringBuilder builder = new StringBuilder();
-		for (RyanairRouteResource resource : resources) {
-			builder.append("Ryanair serves a flight from ").append(resource.getAirportFrom()).append(" to ")
-					.append(resource.getAirportTo()).append("<br />");
-		}
+		// StringBuilder builder = new StringBuilder();
+		// for (int i = 0; i < resources.length; i++) {
+		// builder.append("Ryanair serves a flight from
+		// ").append(resources[i].getAirportFrom()).append(" to ")
+		// .append(resources[i].getAirportTo()).append("<br />");
+		// }
 
 		// HttpResponse<Author> authorResponse =
 		// Unirest.get("http://httpbin.org/books/{id}/author")
@@ -66,8 +41,6 @@ public class InterconnectingFlightsController {
 		// .asObject(Author.class);
 		//
 		// Author authorObject = authorResponse.getBody();
-
-		return builder.toString();
+		return restTemplate.getForObject(ROUTES_ENDPOINT, RyanairRouteResource[].class);
 	}
-
 }
