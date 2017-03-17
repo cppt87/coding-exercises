b5403531bc9088c759dfe0d2fc9949c16bb14a8b
Saverio Campisi <saverio.campisi@unicredit.eu>
2017-03-17 18:10:10 +0100

    Arrays exercises add

--- codility-exercises/src/cppt87/codility/Solutions.java
+++ codility-exercises/src/cppt87/codility/Solutions.java
@@ -1,10 +1,13 @@
 package cppt87.codility;
 
-import cppt87.codility.iterations.Iterations;
+import cppt87.codility.iterations.Arrays;
 
 public class Solutions {
 
 	public static void main(String[] args) {
-		System.out.println(Iterations.binaryGap(345));
+		int A[] = new int[] { 9, 9, 9, 9, 3, 5, 3 };
+		// System.out.println(Iterations.binaryGap(345));
+
+		System.out.println(Arrays.oddOccurrencesInArray(A));
 	}
 }
